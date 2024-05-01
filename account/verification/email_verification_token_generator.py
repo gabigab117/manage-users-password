e3d64f1a898c7f8a08d1738096c6df1738f77a23
gabigab117 <gabrieltrouve5@gmail.com>
2024-05-01 14:07:19 +0200

    replace str() by fstring

--- account/verification/email_verification_token_generator.py
+++ account/verification/email_verification_token_generator.py
@@ -4,9 +4,7 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
 
     def _make_hash_value(self, user, timestamp):
-        return (
-                str(user.is_active) + str(user.pk) + str(timestamp)
-        )
+        return f"{user.is_active}{user.pk}{timestamp}"
 
 
 email_verification_token = EmailVerificationTokenGenerator()
